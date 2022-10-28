8ce91d0cd05c81d4d4f965e8bd736eb3fd2f5f5d
Victoria <47799125+vyk1@users.noreply.github.com>
2022-10-28 20:09:45 -0300

    feat: adds interaction helper

---
+++ utils/create-interaction.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const fs = require('fs')
+
+// The total lines in the .csv file = USERS * INTERECTIONS PER USER
+
+const HEADER = 'Latitude,Longitude,Interaction\n'
+
+const SEPARATOR = ','
+
+const CSV_FILE = `merged.csv`
+
+// Each user trailed x interactions containing <lat, lng>
+const INTERACTIONS = 20
+
+fs.readFile(CSV_FILE, 'utf8', function (err, data) {
+    const DATA = data.split(/\r?\n/)
+
+    let content = ''
+
+    // Removes header and last row
+    DATA.shift()
+    DATA.pop()
+
+    for (let index = 0; index < DATA.length; index++) {
+        content += DATA[index] + SEPARATOR + (index % INTERACTIONS + 1) + '\n'
+    }
+
+    content = content.replace(/^/, HEADER)
+    let filename = 'with-iteractions.csv'
+    fs.appendFileSync(filename, content)
+
+    console.log("Written @" + filename);
+
+})
