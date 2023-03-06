9e091804f91ffd9bd9a8455d931a1632aae72fe9
Victoria <47799125+vyk1@users.noreply.github.com>
2023-03-05 21:56:27 -0300

    feat: adds script to create ficha catalografica

---
+++ ficha-catalografica-ufsc.js
@@ -0,0 +1,19 @@
+/**
+ * @author Victoria Botelho Martins
+ * 
+ * http://ficha.bu.ufsc.br/
+ * 
+ * Script para gerar fichas catalográficas por questões de provisoreidade
+ */
+document.querySelector('[name="nome"]').value="Victoria Botelho"
+document.querySelector('[name="sobrenome"]').value="Martins"
+document.querySelector('[name="titulo"]').value="Analysis"
+document.querySelector('[value="dissertacao"]').click()
+document.querySelector('[name="nome_ori"]').value="Douglas Dyllon Jerônimo"
+document.querySelector('[name="sobrenome_ori"]').value="de Macedo"
+document.querySelector('[name="ano"]').value="2023"
+document.querySelector('[name="pags"]').value="110"
+document.querySelector('[name="assunto2"]').value="Computação Distribuída"
+document.querySelector('[name="assunto3"]').value="Fog Computing"
+document.querySelector('[name="assunto4"]').value="Mobilidade"
+document.querySelector('[name="assunto5"]').value="Sistemas de Localização"
